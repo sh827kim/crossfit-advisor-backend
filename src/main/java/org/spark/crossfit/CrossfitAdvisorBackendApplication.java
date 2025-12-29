c43a44d69b9eb3441c7b7c68df5b64547c8f4a90
10148120 <seonghwa.kim@kt.com>
2025-12-29 17:43:50 +0900

    fix: Railway 배포를 위한 main 메서드 수정

--- src/main/java/org/spark/crossfit/CrossfitAdvisorBackendApplication.java
+++ src/main/java/org/spark/crossfit/CrossfitAdvisorBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 public class CrossfitAdvisorBackendApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(CrossfitAdvisorBackendApplication.class, args);
     }
 
